170433e966c717c706506d083bac38d0f4f65a6a
Abshir <abshir.mohammed@strathmore.edu>
2023-01-25 20:30:38 +0300

    b

--- bank/settings.py
+++ bank/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #new app
+    'clients.apps.ClientsConfig',
 ]
 
 MIDDLEWARE = [
