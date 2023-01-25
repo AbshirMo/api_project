fa9b0ec4d04e704216870a41b1ee5504091d68cb
Abshir <abshir.mohammed@strathmore.edu>
2023-01-25 22:48:12 +0300

    e

--- clients/admin.py
+++ clients/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Branch
+from .models import Branch, Manager, Client
 
 admin.site.register(Branch)
+admin.site.register(Manager)
+admin.site.register(Client)
 # Register your models here.
--- clients/models.py
+++ clients/models.py
@@ -4,8 +4,44 @@ from django.utils import timezone
 
 # Create your models here.
 class Branch(models.Model):
-    branch_name = models.CharField(max_length=50)
+    branch_name = models.CharField(max_length=50, unique=True, help_text='Enter a branch location (e.g Nairobi)')
     address = models.CharField(max_length=50)
+    no_of_emps = models.IntegerField(default=0)
 
     def __str__(self):
         return f'{self.branch_name}'
+
+class Client(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    branch_name = models.ForeignKey(Branch, on_delete=models.RESTRICT)
+    join_date = models.DateTimeField('date launched', null=True)
+    balance = models.IntegerField(default=0)
+
+    KE_SHILLINGS = 'Kshs'
+    DOLLAR = 'Dollar'
+    EURO = 'Euro'
+    BR_POUND = 'Pound'
+
+    CURRENCY_CHOICES = [
+        (KE_SHILLINGS, 'Kenya Shillings'),
+        (DOLLAR, 'US Dollars'),
+        (EURO, 'Euros'),
+        (BR_POUND, 'Pounds')
+    ]
+
+    currency = models.CharField(
+        max_length=10, choices=CURRENCY_CHOICES, default=KE_SHILLINGS)
+
+    def __str__(self):
+        return f'{self.fname} {self.lname}'
+
+class Manager(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    ranch_name = models.ForeignKey(Branch, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return f'{self.fname} {self.lname}'
+
+    
