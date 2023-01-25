aa108bdf0c3fa189c6029058911de42b5da1a545
Abshir <abshir.mohammed@strathmore.edu>
2023-01-25 22:15:57 +0300

    c

--- bank/urls.py
+++ bank/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from clients import views
 
 urlpatterns = [
     path('clients/', include('clients.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
+class Branch(models.Model):
+    branch_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.branch_name}'
